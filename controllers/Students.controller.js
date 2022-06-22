a5bc60638654836bf5faa92f1ef651e6028b3972
Wiynxr <mkx.dake@gmail.com>
2022-06-22 19:38:20 +0300

    Students

--- controllers/Students.controller.js
+++ controllers/Students.controller.js
@@ -2,7 +2,7 @@ const Student = require("../models/Student.model");
 
 module.exports.studentsController = {
   getStudents: (req, res) => {
-    Student.find({ }).then((data) => {
+    Student.find({}).then((data) => {
       res.json(data);
     });
   },
@@ -24,12 +24,11 @@ module.exports.studentsController = {
   },
 
   pathStudents: (req, res) => {
-    Student.findByIdAndUpdate({
-      name: req.body.name,
-      phone: req.body.phone,
-      age: req.body.age,
-    }).then(() => {
-      res.json(`Студент с id ${id} успешно обновлен`);
-    });
+    try {
+      Student.findByIdAndUpdate(req.params.id, { name: req.body.name });
+      res.json("Студент изменен");
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
